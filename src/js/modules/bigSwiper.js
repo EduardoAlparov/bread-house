8719c730f2df40591f97c5a7a133324a86c0a469
EduardoAlparov <workman133@yandex.ru>
2025-02-21 07:28:50 +0300

    fix: modals patterns added

--- src/js/modules/bigSwiper.js
+++ src/js/modules/bigSwiper.js
@@ -1,20 +1,25 @@
 import Swiper from 'swiper';
 import Autoplay from 'swiper/modules/autoplay.mjs';
 import Navigation from 'swiper/modules/navigation.mjs';
+import EffectFade from 'swiper/modules/effect-fade.mjs';
 
 export default () => {
     const bigs = document.querySelectorAll('.js-big-swiper');
 
     bigs.forEach((big) => {
         const isAutoplay = big.dataset.swiper;
+        const isFade = big.dataset.fade;
 
         new Swiper(big, {
             initialSlide: isAutoplay ? 1 : 0,
-            modules: [ Autoplay, Navigation ],
+            modules: [ Autoplay, Navigation, EffectFade ],
             slidesPerView: 1,
             spaceBetween: 10,
-            speed: 1000,
+            speed: isFade ? 600 : 1000,
+
             loop: isAutoplay ? true : false,
+
+            effect: isFade ? 'fade' : 'slide',
 
             autoplay: {
                 enabled: isAutoplay,
